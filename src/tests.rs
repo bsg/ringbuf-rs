3b9bbb4142c03fe0b814c4d2a8a6069fd67b019a
bsg <cem.saldirim@gmail.com>
2026-04-10 15:09:17 +0300

    .

--- src/tests.rs
+++ src/tests.rs
@@ -8,59 +8,20 @@ use std::{
 
 #[test]
 fn it_works() {
-    let mut buf: RingBuf<u8, 7> = Default::default();
+    let mut buf: RingBuf<u8, 15> = Default::default();
 
-    assert_matches!(buf.write(&42u32.to_le_bytes()), Ok(_));
-    assert_matches!(buf.write(&42u8.to_le_bytes()), Ok(_));
-    let mut bytes: [u8; 4] = [0; 4];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u32::from_le_bytes(bytes), 42);
-    let mut bytes: [u8; 1] = [0; 1];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u8::from_le_bytes(bytes), 42);
-
-    assert_matches!(buf.write(&42u32.to_le_bytes()), Ok(_));
-    assert_matches!(buf.write(&42u8.to_le_bytes()), Ok(_));
-    let mut bytes: [u8; 4] = [0; 4];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u32::from_le_bytes(bytes), 42);
-    let mut bytes: [u8; 1] = [0; 1];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u8::from_le_bytes(bytes), 42);
-
-    assert_matches!(buf.write(&42u32.to_le_bytes()), Ok(_));
-    assert_matches!(buf.write(&42u8.to_le_bytes()), Ok(_));
-    let mut bytes: [u8; 4] = [0; 4];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u32::from_le_bytes(bytes), 42);
-    let mut bytes: [u8; 1] = [0; 1];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u8::from_le_bytes(bytes), 42);
-
-    assert_matches!(buf.write(&42u32.to_le_bytes()), Ok(_));
-    assert_matches!(buf.write(&42u8.to_le_bytes()), Ok(_));
-    let mut bytes: [u8; 4] = [0; 4];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u32::from_le_bytes(bytes), 42);
-    let mut bytes: [u8; 1] = [0; 1];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u8::from_le_bytes(bytes), 42);
-
-    assert_matches!(buf.write(&42u32.to_le_bytes()), Ok(_));
-    assert_matches!(buf.write(&42u8.to_le_bytes()), Ok(_));
-    let mut bytes: [u8; 4] = [0; 4];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u32::from_le_bytes(bytes), 42);
-    let mut bytes: [u8; 1] = [0; 1];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u8::from_le_bytes(bytes), 42);
-
-    assert_matches!(buf.write(&42u32.to_le_bytes()), Ok(_));
-    assert_matches!(buf.write(&42u8.to_le_bytes()), Ok(_));
-    let mut bytes: [u8; 4] = [0; 4];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u32::from_le_bytes(bytes), 42);
-    let mut bytes: [u8; 1] = [0; 1];
-    assert_matches!(buf.read_exact(&mut bytes), Ok(_));
-    assert_eq!(u8::from_le_bytes(bytes), 42);
+    (0..1000).for_each(|_| {
+        assert_matches!(buf.write(&42u32.to_le_bytes()), Ok(_));
+        assert_matches!(buf.write(&42u8.to_le_bytes()), Ok(_));
+        assert_matches!(buf.write(&42u64.to_le_bytes()), Ok(_));
+        let mut bytes: [u8; 4] = [0; 4];
+        assert_matches!(buf.read_exact(&mut bytes), Ok(_));
+        assert_eq!(u32::from_le_bytes(bytes), 42);
+        let mut bytes: [u8; 1] = [0; 1];
+        assert_matches!(buf.read_exact(&mut bytes), Ok(_));
+        assert_eq!(u8::from_le_bytes(bytes), 42);
+        let mut bytes: [u8; 8] = [0; 8];
+        assert_matches!(buf.read_exact(&mut bytes), Ok(_));
+        assert_eq!(u64::from_le_bytes(bytes), 42);
+    });
 }
