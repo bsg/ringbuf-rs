9a68bab380314be196e8d7376e22c538a742e6d9
bsg <cem.saldirim@gmail.com>
2026-04-11 13:25:28 +0300

    .

--- src/tests.rs
+++ src/tests.rs
@@ -15,13 +15,13 @@ fn it_works() {
         assert_matches!(buf.write(&42u8.to_le_bytes()), Ok(_));
         assert_matches!(buf.write(&42u64.to_le_bytes()), Ok(_));
         let mut bytes: [u8; 4] = [0; 4];
-        assert_matches!(buf.read_exact(&mut bytes), Ok(_));
+        assert_matches!(buf.read(&mut bytes), Ok(_));
         assert_eq!(u32::from_le_bytes(bytes), 42);
         let mut bytes: [u8; 1] = [0; 1];
-        assert_matches!(buf.read_exact(&mut bytes), Ok(_));
+        assert_matches!(buf.read(&mut bytes), Ok(_));
         assert_eq!(u8::from_le_bytes(bytes), 42);
         let mut bytes: [u8; 8] = [0; 8];
-        assert_matches!(buf.read_exact(&mut bytes), Ok(_));
+        assert_matches!(buf.read(&mut bytes), Ok(_));
         assert_matches!(buf.read(&mut bytes), Ok(0));
     });
 }
